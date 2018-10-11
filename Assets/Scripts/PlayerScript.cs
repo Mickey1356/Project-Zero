66144d4703bd0b9d9e665e4abf9fa00bdba48d47
mar_heave <1003632@users.noreply.github.com>
2018-10-11 21:53:14 +0800

    firstmovements

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public static GameObject player;
+	[SerializeField] private float speed;             //Floating point variable to store the player's movement speed.
+
+	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+	// Use this for initialization
+	void Awake()
+	{
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+		rb2d = GetComponent<Rigidbody2D> ();
+		player = gameObject;
+	}
+
+	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+	void FixedUpdate()
+	{
+		//Store the current horizontal input in the float moveHorizontal.
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		//Store the current vertical input in the float moveVertical.
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Use the two store floats to create a new Vector2 variable movement.
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		float movex = 0;
+		float movey = 0;
+
+
+
+		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+		//rb2d.AddForce (movement * speed);
+		rb2d.velocity = movement * speed;
+		/*if (moveHorizontal == 0f) {
+			movex = 0f;
+		}
+		if (moveVertical == 0f) {
+			movey = 0f;
+		}*/
+		//movement = new Vector2 (movex, movey);
+	}
+}
