7bc48211773a3b1b22023ebe5f6c65ae7c6cb55b
mar_heave <1003632@users.noreply.github.com>
2018-10-17 23:48:02 +0800

    catdie, etc
    restart function when touchedetc

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,8 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
+	private const float timeUntilReset = 3.5f;
+
+	private EntityType type = EntityType.DOG;
+
+	public bool canMove = true;
+	public bool canDie = true;
+	public EntityType Type {
+		get {
+			return type;
+		}
+	}
+
+	private bool dying = false;
+
+	public EntityState state;
 
 	public static GameObject player;
+	public static PlayerScript playerScript;
 	[SerializeField] private float speed;             //Floating point variable to store the player's movement speed.
 
 	private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
@@ -15,6 +31,11 @@ public class PlayerScript : MonoBehaviour {
 		//Get and store a reference to the Rigidbody2D component so that we can access it.
 		rb2d = GetComponent<Rigidbody2D> ();
 		player = gameObject;
+		playerScript = this;
+	}
+
+	void Start() {
+		state = EntityState.ALIVE;
 	}
 
 	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -35,7 +56,9 @@ public class PlayerScript : MonoBehaviour {
 
 		//Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
 		//rb2d.AddForce (movement * speed);
-		rb2d.velocity = movement * speed;
+		if (canMove) {
+			rb2d.velocity = movement * speed;
+		}
 		/*if (moveHorizontal == 0f) {
 			movex = 0f;
 		}
@@ -43,5 +66,33 @@ public class PlayerScript : MonoBehaviour {
 			movey = 0f;
 		}*/
 		//movement = new Vector2 (movex, movey);
+	}
+
+	void Update() {
+		if (state == EntityState.DEAD && dying == false ) {
+			Die();
+			dying = true;
+		}
+	}
+
+	void Die() {
+		Debug.Log ("The dog has died!"); //different messages?
+		canMove = false;
+		//death animation, statistics etc.
+		//wait for some time
+		Debug.Log("Restarting level...");
+		//yield return new WaitForSeconds(timeUntilReset); //don't know how to get this to work.
+		//reset game
+		GameManager.gameManager.RestartLevel ();
+	}
+
+	public void PlayerTouched(GameObject obj = null) {
+		Debug.Log ("received");
+		if (obj != null && obj.tag == "Cat" && obj.GetComponent<CatController> () != null) {
+			if (obj.GetComponent<CatController> ().EntType == EntityType.BIGCAT && canDie == true) {
+				state = EntityState.DEAD;
+
+			}	
+		}
 	}
 }
